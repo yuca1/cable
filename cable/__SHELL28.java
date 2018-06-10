215ca4968b7cce2ad1dc91502d1ad43b8b7de6ed
yuca1 <yucai@vassar.edu>
2018-06-10 15:26:32 -0400

    added 1988 Indiana

---
+++ cable/__SHELL28.java
@@ -0,0 +1,10 @@
+
+public class __SHELL28 extends bluej.runtime.Shell {
+public static void run() throws Throwable {
+final bluej.runtime.BJMap __bluej_runtime_scope = getScope("/Volumes/SanDisk/Cable/cable");
+final Cable cable1 = (Cable)__bluej_runtime_scope.get("cable1");
+
+
+cable1.test();
+
+}}
